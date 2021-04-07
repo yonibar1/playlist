299f7b1789d9a9d644c5221021c1e1532433b5d8
yonibar1 <yonibar1999@gmail.com>
2021-04-07 14:13:00 +0300

    Yoni first commit

---
+++ src/cmps/PlayListFilter/index.js
@@ -0,0 +1,1 @@
+export { PlayListFilter } from './PlayListFilter'
---
+++ src/cmps/PlayListPlayer/PlayListPlayer.jsx
@@ -0,0 +1,15 @@
+
+import { Component } from 'react'
+
+import './PlayListPlayer.scss'
+
+export class PlayListPlayer extends Component {
+
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
