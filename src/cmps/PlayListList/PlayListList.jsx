63c8a58f3b83eccb2cf7e49bb6fc302b5bf1525b
yonibar1 <yonibar1999@gmail.com>
2021-04-07 15:14:35 +0300

    Yoni commit

--- src/cmps/PlayListList/PlayListList.jsx
+++ src/cmps/PlayListList/PlayListList.jsx
@@ -3,12 +3,12 @@ import { PlayListPreview } from '../PlayListPreview'
 
 import './PlayListList.scss'
 
-export function PlayListList({ videos }) {
+export function PlayListList({ videos,onSelectVideo }) {
     return (
         <div>
             <h1>List</h1>
             <ul>
-                {videos ? videos.map((video, idx) => <li key={idx}><PlayListPreview video={video} /></li>) : <div>Loading...</div>}
+                {videos ? videos.map((video, idx) => <li key={idx}><PlayListPreview onSelectVideo={onSelectVideo} video={video} /></li>) : <div>Loading...</div>}
             </ul>
 
         </div>
--- src/views/PlayListApp/PlayListApp.jsx
+++ src/views/PlayListApp/PlayListApp.jsx
@@ -9,7 +9,9 @@ import { youtubeService } from '../../services/youtube.service'
 
 export class PlayListApp extends Component {
     state = {
-        videos: null
+        videos: null,
+        videoId:null,
+        songName:null
     }
     componentDidMount() {
         this.loadVideos()
@@ -22,13 +24,17 @@ export class PlayListApp extends Component {
         console.log(filterBy);
         // this.setState({ filterBy }, () => this.loadContacts())
     }
+    onSelectVideo = (videoId,songName) =>{
+        console.log('songName',songName,videoId,'VIDEO ID');
+        this.setState({videoId,songName})
+    }
 
     render() {
         const { videos } = this.state
         return (
             <div>
                 <PlayListFilter onChangeFilter={this.onChangeFilter} />
-                <PlayListList videos={videos} />
+                <PlayListList onSelectVideo={this.onSelectVideo} videos={videos} />
                 <PlayListPlayer />
             </div>
         )
