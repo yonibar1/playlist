95937a274fd0f2755fce94844b3215462bdfb5a9
AriGabay <arigabay19988@gmail.com>
2021-04-07 16:33:50 +0300

    Ari Commit

--- src/cmps/PlayListPreview/PlayListPreview.jsx
+++ src/cmps/PlayListPreview/PlayListPreview.jsx
@@ -1,18 +1,23 @@
+import { Component } from 'react';
 
-import { Component } from 'react'
+import './PlayListPreview.scss';
 
-import './PlayListPreview.scss'
-
-export function PlayListPreview({ video,onSelectVideo }) {
-    return (
-        <div className="playlist-preview">
-            <img src={video.snippet.thumbnails.default.url} alt=""/>
-            <div className="video-details">
-                <h3>{video.snippet.title}</h3>
-                <p>{video.snippet.description}</p>
-                <p>{video.snippet.publishedAt}</p>
-            </div>
-            <button onClick={()=>onSelectVideo(video.id.videoId,video.snippet.title)}>Play</button>
+export function PlayListPreview({ video, onSelectVideo }) {
+  return (
+    <div className="playlist-preview">
+      <div className="img-container">
+        <img src={video.snippet.thumbnails.medium.url} alt="" />
+      </div>
+      <div className="playlist-preview-container">
+        <div className="video-details">
+          <h3>{video.snippet.title}</h3>
+          <p>{video.snippet.description}</p>
+          <p>{video.snippet.publishedAt}</p>
         </div>
-    )
+      </div>
+      <button className="play-video" onClick={() => onSelectVideo(video.id.videoId, video.snippet.title)}>
+        Play
+      </button>
+    </div>
+  );
 }
